63cf96bfe6efca394ff9e1555dcee568cb8d8406
cesarel77 <cesar.onate04102006@gmail.com>
2026-04-20 23:23:00 -0400

    create text and alert

---
+++ src/javascript/02-texto.js
@@ -0,0 +1,13 @@
+
+//Definimos variables con var
+var nombre = "Cesar";
+var apellido = "Oñate";
+var edad = 33;
+
+//Mostrar texto en consola.log
+console.log(nombre + " " + apellido, + " " + edad);
+console.log(nombre, apellido, edad);
+
+var nom_ape = nombre + " " + apellido;
+console.log(nom_ape, " " + edad );
+
