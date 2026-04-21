3ae80fc9080399731cb66a8b5d36aaf7f1161109
cesarel77 <cesar.onate04102006@gmail.com>
2026-04-20 22:54:59 -0400

    create a index, style and JS
    En esta noche aprendimos hacer matematicas con javascript

---
+++ src/javascript/01-matematicas.js
@@ -0,0 +1,49 @@
+
+//definimos las bariables
+let x = 2;
+let y = 4;
+
+//mostrar el valor de cada variable
+console.log('La variable de "X" tiene un valor de: ',x);
+console.log("La variable de 'Y' tiene un valor de: ",y);
+
+console.log("----------------------------------------");
+
+//operaciones matematicas
+console.log("Suma: ", x + y);
+console.log("Resta: ", x - y);
+console.log("Divición: ", x / y);
+console.log("Multiplicación: ", x * y);
+
+//transformamos el int en un string
+console.log("Parse: " + (x * y));
+
+//Definimos BooLeans
+let t = true;
+let f = false;
+
+//comparativa de valores BooLeanos
+console.log("Si comparamos t con t nos da: ", t == t);
+console.log("Si comparamos t con f nos da: ", t == f);
+console.log("Si comparamos f con f nos da: ", f == f);
+
+console.log("---------------------------------------------");
+
+//comparativa una u otra
+console.log("Si es t ó t nos da: ", t || t);
+console.log("Si es t ó f nos da: ", t || f);
+console.log("Si es f ó f nos da: ", f || f);
+
+console.log("---------------------------------------------");
+//cumplir dos condiciones
+console.log("Si elegimos entre t y t tenemos: ", t && t);
+console.log("Si elegimos entre t y f tenemos: ", t && f);
+console.log("Si elegimos entre f y f tenemos: ", f && f);
+
+console.log("---------------------------------------------");
+//Distinto
+console.log("Si t es distinto de t nos da: ", t != t);
+console.log("Si t es distinto de f nos da: ", t != f);
+console.log("Si f es distinto de f nos da: ", f != f);
+
+    
